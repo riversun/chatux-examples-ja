4be905ef8662dab8b965e3672aef20de9a763758
riversun <riversun.org@gmail.com>
2019-03-31 19:36:34 +0900

    add server support for POST method

--- step03/chatserver/server.js
+++ step03/chatserver/server.js
@@ -1,54 +1,70 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const port = 8080;
+const port = 8081;
 // CORSを有効にする
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
     next();
 });
+
+//POSTでrequest bodyをJSONとして受け取るとき
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+function handleRequest(userInputText, callback, res) {
+    const response = {output: []};
+    const msg = response.output;
+
+    if (userInputText == 'ボタン') {
+        msg.push({
+            type: 'text',
+            value: '好きな動物は？',
+            delayMs: 500 //表示ディレイ（ミリ秒）
+        });
+        //オプションボタンを作る
+        const opts = [];
+        opts.push({label: 'イヌ', value: '犬'});
+        opts.push({label: 'ネコ', value: '猫'});
+        opts.push({label: 'ウサギ', value: '兎'});
+        msg.push({type: 'option', options: opts});
+    } else if (userInputText == '画像') {
+        msg.push({
+            type: 'text',
+            value: '画像を表示します',
+            delayMs: 500
+        });
+        msg.push({
+            type: 'image',
+            value: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Kaiserpinguinjunges.jpg/800px-Kaiserpinguinjunges.jpg'
+        });
+    } else {
+        msg.push({
+            type: 'text',
+            value: '「' + userInputText + '」ですね！'
+        });
+    }
+
+    if (callback) {
+        const responseText = callback + '(' + JSON.stringify(response) + ')';
+        res.set('Content-Type', 'application/javascript');
+        res.send(responseText);
+    } else {
+        res.json(response);
+    }
+}
+
 app.get('/chat', function (req, res) {
         const userInputText = req.query.text;
         const callback = req.query.callback;
-        const response = {output: []};
-        const msg = response.output;
-
-        if (userInputText == 'ボタン') {
-            msg.push({
-                type: 'text',
-                value: '好きな動物は？',
-                delayMs: 500 //表示ディレイ（ミリ秒）
-            });
-            //オプションボタンを作る
-            const opts = [];
-            opts.push({label: 'イヌ', value: '犬'});
-            opts.push({label: 'ネコ', value: '猫'});
-            opts.push({label: 'ウサギ', value: '兎'});
-            msg.push({type: 'option', options: opts});
-        } else if (userInputText == '画像') {
-            msg.push({
-                type: 'text',
-                value: '画像を表示します',
-                delayMs: 500
-            });
-            msg.push({
-                type: 'image',
-                value: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Kaiserpinguinjunges.jpg/800px-Kaiserpinguinjunges.jpg'
-            });
-        } else {
-            msg.push({
-                type: 'text',
-                value: '「' + userInputText + '」ですね！'
-            });
-        }
-
-        if (callback) {
-            const responseText = callback + '(' + JSON.stringify(response) + ')';
-            res.set('Content-Type', 'application/javascript');
-            res.send(responseText);
-        } else {
-            res.json(response);
-        }
+        handleRequest(userInputText, callback, res);
+    }
+);
+app.post('/chat', function (req, res) {
+        const userInputText = req.body.text;
+        const callback = null;
+        handleRequest(userInputText, callback, res);
     }
 );
 app.listen(port, () => {
